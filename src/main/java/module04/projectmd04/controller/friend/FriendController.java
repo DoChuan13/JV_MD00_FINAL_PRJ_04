99f8bb136046ea4967440d2881c077d29790bc00
DoChuan13 <chuando.9213@gmail.com>
2023-05-16 17:31:08 +0700

    23/05/16 Dev: Fixed GUI

--- src/main/java/module04/projectmd04/controller/friend/FriendController.java
+++ src/main/java/module04/projectmd04/controller/friend/FriendController.java
@@ -1,8 +1,13 @@
 package module04.projectmd04.controller.friend;
 
+import module04.projectmd04.config.detail.JSPLink;
+import module04.projectmd04.config.detail.URL;
+import module04.projectmd04.model.User;
 import module04.projectmd04.service.Services;
 import module04.projectmd04.service.friend.IFriendService;
+import module04.projectmd04.service.user.IUserService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +18,7 @@ import java.io.IOException;
 @WebServlet("/friend")
 public class FriendController extends HttpServlet {
     private static final IFriendService friendService = Services.getInstance().getFriendService();
+    private static final IUserService userService = Services.getInstance().getUserService();
 
     public FriendController() {
     }
@@ -27,8 +33,12 @@ public class FriendController extends HttpServlet {
         if (action == null) {
             action = "";
         }
-
+        switch (action) {
+            default:
+                showFriendPage(request,response);
+        }
     }
+
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -39,6 +49,24 @@ public class FriendController extends HttpServlet {
 
         if (action == null) {
             action = "";
+        }
+    }
+
+    private void showFriendPage(HttpServletRequest request, HttpServletResponse response) {
+        User currentUser = userService.getCurrentUser(request);
+        if (currentUser == null) {
+            try {
+                response.sendRedirect(URL.PATH_USER_LOGIN);
+                return;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        RequestDispatcher dispatcher = request.getRequestDispatcher(JSPLink.PATH_USER_FRIEND);
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/webapp/WEB-INF/chat/chat.jsp
+++ src/main/webapp/WEB-INF/chat/chat.jsp
@@ -6,6 +6,9 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<jsp:include page="../bootstrap/nav-bar.jsp">
+    <jsp:param name="articleId" value=""/>
+</jsp:include>
 <html>
 <head>
     <title>Title</title>
